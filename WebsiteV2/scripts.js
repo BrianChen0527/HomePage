7f73af33691301f4d083492c275b6aabd398cae4
BrianChen0527 <brianc.boston@gmail.com>
2022-06-28 17:05:54 +0800

    adding two truths one lie portion

--- WebsiteV2/scripts.js
+++ WebsiteV2/scripts.js
@@ -2,3 +2,46 @@ function dropMenu(x) {
   x.classList.toggle("change");
 }
 
+function vertical_bar_animation(){
+  const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+      const square = entry.target.querySelector('.vertical_bar');
+
+      if (entry.isIntersecting) {
+        square.classList.add('vertical_expansion');
+      return; // if we added the class, exit the function
+      }
+
+      // We're not intersecting, so remove the class!
+      square.classList.remove('vertical_expansion');
+    });
+  });
+
+  observer.observe(document.querySelector('.AboutMe'));
+  observer.observe(document.querySelector('.Experience'));
+}
+
+function horizontal_bar_animation(){
+  const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+      const square = entry.target.querySelector('.horizontal_bar');
+
+      if (entry.isIntersecting) {
+        square.classList.add('horizontal_expansion');
+      return; // if we added the class, exit the function
+      }
+
+      // We're not intersecting, so remove the class!
+      square.classList.remove('horizontal_expansion');
+    });
+  });
+
+  observer.observe(document.querySelector('.socials'));
+}
+
+window.addEventListener("scroll", vertical_bar_animation);
+window.addEventListener("scroll", horizontal_bar_animation);
+
+// To check the scroll position on page load
+vertical_bar_animation();
+horizontal_bar_animation();
