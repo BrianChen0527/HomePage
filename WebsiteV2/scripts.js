a80a269c267223fd9012919fb41a05016d9be781
BrianChen0527 <brianc.boston@gmail.com>
2022-06-30 15:06:17 +0800

    done with index, working on projects page

--- WebsiteV2/scripts.js
+++ WebsiteV2/scripts.js
@@ -1,7 +1,7 @@
 /*
 #####################################################################################################################
 #####################################################################################################################
-################################################ bar-related animation ##############################################
+################################################ text fly-in animation ##############################################
 #####################################################################################################################
 #####################################################################################################################
 */
@@ -23,7 +23,13 @@ function fly_in_animation(){
 
   observer.observe(document.querySelector('.title'));
 }
-
+/*
+#####################################################################################################################
+#####################################################################################################################
+################################################ bar-related animation ##############################################
+#####################################################################################################################
+#####################################################################################################################
+*/
 function vertical_bar_animation(){
   const observer = new IntersectionObserver(entries => {
     entries.forEach(entry => {
@@ -61,12 +67,32 @@ function horizontal_bar_animation(){
   observer.observe(document.querySelector('.socials'));
 }
 
+function horizontal_bar_animation2(){
+  const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+      const square = entry.target.querySelector('.horizontal_bar');
+
+      if (entry.isIntersecting) {
+        square.classList.add('horizontal_expansion');
+      return; // if we added the class, exit the function
+      }
+
+      // We're not intersecting, so remove the class!
+      square.classList.remove('horizontal_expansion');
+    });
+  });
+
+  observer.observe(document.querySelector('#projectsWrap'));
+}
+
 
 window.addEventListener("scroll", fly_in_animation);
 window.addEventListener("scroll", vertical_bar_animation);
 window.addEventListener("scroll", horizontal_bar_animation);
+window.addEventListener("scroll", horizontal_bar_animation2);
 
 // To check the scroll position on page load
 fly_in_animation()
 vertical_bar_animation();
 horizontal_bar_animation();
+horizontal_bar_animation2();
